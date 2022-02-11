c2991f8d594e7c4620824f3d350d9d7123c1c921
Alix <alixwagner@yahoo.ca>
2022-02-10 19:07:15 -0500

    initial commit

---
+++ javascript/app.js
@@ -0,0 +1,22 @@
+// JS -
+
+//     Create an app object(cocktailConnoisseur)
+
+// Initialize preset data in the dedicated properties
+// apiURL
+// apiKey
+
+// eventhandler submit button:
+//     prevent default
+//     get input from alcohol input
+//     use input to create search parameters and Fetch from API 
+//     take returned API response object
+//     create JSON object
+//     Access JSON object for cocktail
+//         image
+//         name 
+//         recipe
+// instructions
+//     print returned data to page
+
+// Create an init method 
