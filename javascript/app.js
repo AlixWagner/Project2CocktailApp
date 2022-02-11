8135fbc21a29af7859658928ed7d6c505af1c0b9
Alix <alixwagner@yahoo.ca>
2022-02-11 11:40:09 -0500

    updated pseudo code

--- javascript/app.js
+++ javascript/app.js
@@ -9,14 +9,33 @@
 // eventhandler submit button:
 //     prevent default
 //     get input from alcohol input
-//     use input to create search parameters and Fetch from API 
+//     use input to create search parameters and Fetch from API
+//     take returned API response object
+//     create JSON object
+//     Use function to randomly select one returned Drink from the list
+//     Access JSON object for cocktail
+//         id
+//         name
+//     print returned data to page
+
+// eventhandler try again button:
+//     prevent default
+//     Use function to randomly select another returned Drink from the list
+//     Access JSON object for cocktail
+//         id
+//         name
+//     print returned data to page
+
+// eventhandler Make This Drink button:
+//     prevent default
+//     get id from selected drink
+//     use input to create search parameters and Fetch from API
 //     take returned API response object
 //     create JSON object
 //     Access JSON object for cocktail
-//         image
-//         name 
-//         recipe
-// instructions
+//         ingredients
+//         measurements
+//         instructions
 //     print returned data to page
 
 // Create an init method 
